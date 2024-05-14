0fae05c81157f1c1071c6e54f7a13b2fd4a9caef
TemmyJewel <temmyjewel22@gmail.com>
2024-05-14 10:11:52 +0100

    commiting

---
+++ Java1/index.js
@@ -0,0 +1,62 @@
+// console.log('Hello World');
+
+// alert('Welcome Everyone')
+//         prompt('Please enter your age')
+//         confirm('Are you sure you want to pay')
+
+//datatype are primitive an non-primitive
+//Primitive/basic datatypes are string, numbers, booleans, null, undefined
+//Non-primitve are Arrays, objrcts, dates, functions, regex e.t.c
+
+//Variables are spaces that store data. To create variables, you use var , let, const
+//let: allows to reassign a variable's value after declaration.
+//const: does not allow to reassign a variable's value after declaration.
+//Var: not longer used because it's allows for leakage of data.
+
+//Basic Datatypes
+//Number
+// let age = 30
+// age = 15
+// console.log(age);
+
+// //Strings
+// let name ='Marinna'
+// console.log(name)
+
+// //Booleans
+// let isMarried = true
+// console.log(isMarried);
+
+// //Null
+// let student = null
+// console.log(student);
+
+// //Undefined
+// let stu;
+// console.log(stu);
+
+//Array
+//Two things are integral to an array; length and index
+//Length is the total number of items or elements int array, start counting from 1
+//The index is the position of items or elements in an array, start counting from 0
+let colors = ['red', 'yellow', 'blue', 'white']
+// console.log(colors.length);
+
+
+
+// colors.push('wine', 'purple', 'orange', 'green', 'violent');
+
+// console.log(colors);
+// console.log(colors[5]);
+
+
+// colors.pop();
+// console.log(colors);
+
+
+// colors.shift();
+// console.log(colors);
+
+colors.unshift('grey')
+console.log(colors);
+
